e022dc5c07e0a59494546593244eb5e7118796ac
Justin Britt <britt@uwindsor.ca>
2021-04-26 12:09:12 -0400

    Retarget project

---
+++ VanOostrum2011/Classes/IsExternalInit.cs
@@ -0,0 +1,21 @@
+﻿// https://blog.ndepend.com/using-c9-record-and-init-property-in-your-net-framework-4-x-net-standard-and-net-core-projects/
+namespace ClassLibrary
+{
+    public record Class(string Str)
+    {
+        internal int Int { get; init; }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    using System.ComponentModel;
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
