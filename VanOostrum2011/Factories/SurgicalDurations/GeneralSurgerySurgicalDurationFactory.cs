60770208da62ba03fcf3b6d2c2d2230e1d4d99a7
Justin Britt <britt@uwindsor.ca>
2022-04-03 14:16:56 -0400

    Added factory

---
+++ VanOostrum2011/Factories/SurgicalDurations/GeneralSurgerySurgicalDurationFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace VanOostrum2011.Factories.SurgicalDurations
+{
+    using VanOostrum2011.Interfaces.SurgicalDurations;
+    using VanOostrum2011.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using VanOostrum2011.InterfacesFactories.SurgicalDurations;
+    using VanOostrum2011.Records.SurgicalDurations;
+
+    internal sealed class GeneralSurgerySurgicalDurationFactory : IGeneralSurgerySurgicalDurationFactory
+    {
+        public GeneralSurgerySurgicalDurationFactory()
+        {
+        }
+
+        public ISurgicalDuration Create(
+            IDurationFactory durationFactory)
+        {
+            IGeneralSurgerySurgicalDuration surgicalDuration = null;
+
+            try
+            {
+                surgicalDuration = new GeneralSurgerySurgicalDuration(
+                    durationFactory);
+            }
+            finally
+            {
+            }
+
+            return surgicalDuration;
+        }
+    }
+}
