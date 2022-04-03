c714167973ea99c4f11c35d409d97bfdf001398e
Justin Britt <britt@uwindsor.ca>
2022-04-03 15:52:05 -0400

    Added factory

---
+++ VanOostrum2011/Factories/PatientLengthOfStays/GeneralSurgeryPatientLengthOfStayFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace VanOostrum2011.Factories.PatientLengthOfStays
+{
+    using VanOostrum2011.Interfaces.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using VanOostrum2011.InterfacesFactories.PatientLengthOfStays;
+    using VanOostrum2011.Records.PatientLengthOfStays;
+
+    internal sealed class GeneralSurgeryPatientLengthOfStayFactory : IGeneralSurgeryPatientLengthOfStayFactory
+    {
+        public GeneralSurgeryPatientLengthOfStayFactory()
+        {
+        }
+
+        public IPatientLengthOfStay Create(
+            IDurationFactory durationFactory)
+        {
+            IGeneralSurgeryPatientLengthOfStay patientLengthOfStay = null;
+
+            try
+            {
+                patientLengthOfStay = new GeneralSurgeryPatientLengthOfStay(
+                    durationFactory);
+            }
+            finally
+            {
+            }
+
+            return patientLengthOfStay;
+        }
+    }
+}
