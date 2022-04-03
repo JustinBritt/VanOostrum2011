2d99b8f398ef4bffaa99f3aa10aa6f38e8b7b5fb
Justin Britt <britt@uwindsor.ca>
2022-04-03 03:44:57 -0400

    Added factory

---
+++ VanOostrum2011/Factories/SurgicalDurations/EarNoseThroatSurgerySurgicalDurationFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace VanOostrum2011.Factories.SurgicalDurations
+{
+    using VanOostrum2011.Interfaces.SurgicalDurations;
+    using VanOostrum2011.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using VanOostrum2011.InterfacesFactories.SurgicalDurations;
+    using VanOostrum2011.Records.SurgicalDurations;
+
+    internal sealed class EarNoseThroatSurgerySurgicalDurationFactory : IEarNoseThroatSurgerySurgicalDurationFactory
+    {
+        public EarNoseThroatSurgerySurgicalDurationFactory()
+        {
+        }
+
+        public ISurgicalDuration Create(
+            IDurationFactory durationFactory)
+        {
+            IEarNoseThroatSurgerySurgicalDuration surgicalDuration = null;
+
+            try
+            {
+                surgicalDuration = new EarNoseThroatSurgerySurgicalDuration(
+                    durationFactory);
+            }
+            finally
+            {
+            }
+
+            return surgicalDuration;
+        }
+    }
+}
