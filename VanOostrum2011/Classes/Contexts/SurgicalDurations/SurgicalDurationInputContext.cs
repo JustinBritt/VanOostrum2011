53bbd91e99a168a1d4c14b6b5a201fb14d8af1e6
Justin Britt <britt@uwindsor.ca>
2022-04-03 17:53:31 -0400

    Added class

---
+++ VanOostrum2011/Classes/Contexts/SurgicalDurations/SurgicalDurationInputContext.cs
@@ -0,0 +1,22 @@
+﻿namespace VanOostrum2011.Classes.Contexts.SurgicalDurations
+{
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.Interfaces.Contexts.SurgicalDurations;
+
+    internal sealed class SurgicalDurationInputContext : ISurgicalDurationInputContext
+    {
+        public SurgicalDurationInputContext(
+            CodeableConcept specialty,
+            IValue<string> statistic)
+        {
+            this.Specialty = specialty;
+
+            this.Statistic = statistic;
+        }
+
+        public CodeableConcept Specialty { get; }
+
+        public IValue<string> Statistic { get; }
+    }
+}
---
+++ VanOostrum2011/Interfaces/Contexts/SurgicalDurations/ISurgicalDurationInputContext.cs
@@ -0,0 +1,11 @@
+﻿namespace VanOostrum2011.Interfaces.Contexts.SurgicalDurations
+{
+    using Hl7.Fhir.Model;
+
+    public interface ISurgicalDurationInputContext
+    {
+        CodeableConcept Specialty { get; }
+
+        IValue<string> Statistic { get; }
+    }
+}
