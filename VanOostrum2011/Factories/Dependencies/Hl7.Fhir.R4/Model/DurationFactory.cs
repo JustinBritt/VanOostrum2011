df82d18d0b8b60f1e7571d7201230c1ba47d3313
Justin Britt <britt@uwindsor.ca>
2022-04-03 02:10:19 -0400

    Added factory

---
+++ VanOostrum2011/Factories/Dependencies/Hl7.Fhir.R4/Model/DurationFactory.cs
@@ -0,0 +1,63 @@
+﻿namespace VanOostrum2011.Factories.Dependencies.Hl7.Fhir.R4.Model
+{
+    using global::Hl7.Fhir.Model;
+
+    using VanOostrum2011.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    internal sealed class DurationFactory : IDurationFactory
+    {
+        private const string unitsofmeasure = "http://unitsofmeasure.org";
+
+        public DurationFactory()
+        {
+        }
+
+        public Duration Create()
+        {
+            Duration duration;
+
+            try
+            {
+                duration = new Duration();
+            }
+            finally
+            {
+            }
+
+            return duration;
+        }
+
+        public Duration Create(
+            string unit,
+            decimal value)
+        {
+            return this.Create(
+                unitsofmeasure,
+                unit,
+                value);
+        }
+
+        public Duration Create(
+            string system,
+            string unit,
+            decimal value)
+        {
+            Duration duration = null;
+
+            try
+            {
+                duration = new Duration()
+                {
+                    System = system,
+                    Unit = unit,
+                    Value = value
+                };
+            }
+            finally
+            {
+            }
+
+            return duration;
+        }
+    }
+}
---
+++ VanOostrum2011/InterfacesFactories/Dependencies/Hl7.Fhir.R4/Model/IDurationFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace VanOostrum2011.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model
+{
+    using global::Hl7.Fhir.Model;
+
+    public interface IDurationFactory
+    {
+        Duration Create();
+
+        Duration Create(
+            string unit,
+            decimal value);
+
+        Duration Create(
+            string system,
+            string unit,
+            decimal value);
+    }
+}
