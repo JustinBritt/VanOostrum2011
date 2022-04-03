046fb63f33f53025cfc769710867ddf9a9aa2023
Justin Britt <britt@uwindsor.ca>
2022-04-03 18:46:55 -0400

    Added class

---
+++ VanOostrum2011/Classes/Contexts/SurgicalDurations/SurgicalDurationOutputContext.cs
@@ -0,0 +1,17 @@
+﻿namespace VanOostrum2011.Classes.Contexts.SurgicalDurations
+{
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.Interfaces.Contexts.SurgicalDurations;
+
+    internal sealed class SurgicalDurationOutputContext : ISurgicalDurationOutputContext
+    {
+        public SurgicalDurationOutputContext(
+            Duration duration)
+        {
+            this.Duration = duration;
+        }
+
+        public Duration Duration { get; }
+    }
+}
---
+++ VanOostrum2011/Interfaces/Contexts/SurgicalDurations/ISurgicalDurationOutputContext.cs
@@ -0,0 +1,9 @@
+﻿namespace VanOostrum2011.Interfaces.Contexts.SurgicalDurations
+{
+    using Hl7.Fhir.Model;
+
+    public interface ISurgicalDurationOutputContext
+    {
+        Duration Duration { get; }
+    }
+}
