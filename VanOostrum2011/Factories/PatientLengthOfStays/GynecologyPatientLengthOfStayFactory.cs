7a58237905ac0483ff7ca7bc73ced26208ce6560
Justin Britt <britt@uwindsor.ca>
2022-04-03 15:57:31 -0400

    Added factory

---
+++ VanOostrum2011/Factories/PatientLengthOfStays/GynecologyPatientLengthOfStayFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace VanOostrum2011.Factories.PatientLengthOfStays
+{
+    using VanOostrum2011.Interfaces.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using VanOostrum2011.InterfacesFactories.PatientLengthOfStays;
+    using VanOostrum2011.Records.PatientLengthOfStays;
+
+    internal sealed class GynecologyPatientLengthOfStayFactory : IGynecologyPatientLengthOfStayFactory
+    {
+        public GynecologyPatientLengthOfStayFactory()
+        {
+        }
+
+        public IPatientLengthOfStay Create(
+            IDurationFactory durationFactory)
+        {
+            IGynecologyPatientLengthOfStay patientLengthOfStay = null;
+
+            try
+            {
+                patientLengthOfStay = new GynecologyPatientLengthOfStay(
+                    durationFactory);
+            }
+            finally
+            {
+            }
+
+            return patientLengthOfStay;
+        }
+    }
+}
