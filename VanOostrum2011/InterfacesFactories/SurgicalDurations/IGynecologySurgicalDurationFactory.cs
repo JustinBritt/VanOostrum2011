1aa98979b1a3478476a91366db5c132bf24550d0
Justin Britt <britt@uwindsor.ca>
2022-04-03 14:24:35 -0400

    Added factory

---
+++ VanOostrum2011/InterfacesFactories/SurgicalDurations/IGynecologySurgicalDurationFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace VanOostrum2011.InterfacesFactories.SurgicalDurations
+{
+    using VanOostrum2011.Interfaces.SurgicalDurations;
+    using VanOostrum2011.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    public interface IGynecologySurgicalDurationFactory
+    {
+        ISurgicalDuration Create(
+            IDurationFactory durationFactory);
+    }
+}
