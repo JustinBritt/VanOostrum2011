db7ddc81e764adbfbe026a70f74cffabe26d6baf
Justin Britt <britt@uwindsor.ca>
2022-04-03 19:31:41 -0400

    Added class

---
+++ VanOostrum2011/Classes/Contexts/PatientLengthOfStays/PatientLengthOfStayInputContext.cs
@@ -0,0 +1,22 @@
+﻿namespace VanOostrum2011.Classes.Contexts.PatientLengthOfStays
+{
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.Interfaces.Contexts.PatientLengthOfStays;
+
+    internal sealed class PatientLengthOfStayInputContext : IPatientLengthOfStayInputContext
+    {
+        public PatientLengthOfStayInputContext(
+            CodeableConcept specialty,
+            IValue<string> statistic)
+        {
+            this.Specialty = specialty;
+
+            this.Statistic = statistic;
+        }
+
+        public CodeableConcept Specialty { get; }
+
+        public IValue<string> Statistic { get; }
+    }
+}
---
+++ VanOostrum2011/Interfaces/Contexts/PatientLengthOfStays/IPatientLengthOfStayInputContext.cs
@@ -0,0 +1,11 @@
+﻿namespace VanOostrum2011.Interfaces.Contexts.PatientLengthOfStays
+{
+    using Hl7.Fhir.Model;
+
+    public interface IPatientLengthOfStayInputContext
+    {
+        CodeableConcept Specialty { get; }
+
+        IValue<string> Statistic { get; }
+    }
+}
