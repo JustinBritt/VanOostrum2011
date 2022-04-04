ff4a420260a6e04872c312404ccf949b482b7af5
Justin Britt <britt@uwindsor.ca>
2022-04-03 21:01:38 -0400

    Added factory

---
+++ VanOostrum2011/Factories/Contexts/PatientLengthOfStays/PatientLengthOfStayInputContextFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace VanOostrum2011.Factories.Contexts.PatientLengthOfStays
+{
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.Classes.Contexts.PatientLengthOfStays;
+    using VanOostrum2011.Interfaces.Contexts.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesFactories.Contexts.PatientLengthOfStays;
+
+    internal sealed class PatientLengthOfStayInputContextFactory : IPatientLengthOfStayInputContextFactory
+    {
+        public PatientLengthOfStayInputContextFactory()
+        {
+        }
+
+        public IPatientLengthOfStayInputContext Create(
+            CodeableConcept specialty,
+            IValue<string> statistic)
+        {
+            IPatientLengthOfStayInputContext context = null;
+
+            try
+            {
+                context = new PatientLengthOfStayInputContext(
+                    specialty,
+                    statistic);
+            }
+            finally
+            {
+            }
+
+            return context;
+        }
+    }
+}
---
+++ VanOostrum2011/InterfacesFactories/Contexts/PatientLengthOfStays/IPatientLengthOfStayInputContextFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace VanOostrum2011.InterfacesFactories.Contexts.PatientLengthOfStays
+{
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.Interfaces.Contexts.PatientLengthOfStays;
+
+    public interface IPatientLengthOfStayInputContextFactory
+    {
+        IPatientLengthOfStayInputContext Create(
+            CodeableConcept specialty,
+            IValue<string> statistic);
+    }
+}
