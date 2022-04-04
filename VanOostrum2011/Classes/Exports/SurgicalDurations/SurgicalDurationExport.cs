0214ab8fa702372ae2e8cf9ca01caa29951fa3e3
Justin Britt <britt@uwindsor.ca>
2022-04-04 17:37:43 -0400

    Added class

---
+++ VanOostrum2011/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -0,0 +1,28 @@
+﻿namespace VanOostrum2011.Classes.Exports.SurgicalDurations
+{
+    using VanOostrum2011.Extensions.SurgicalDurations;
+    using VanOostrum2011.Interfaces.Contexts.SurgicalDurations;
+    using VanOostrum2011.Interfaces.Exports.SurgicalDurations;
+    using VanOostrum2011.InterfacesAbstractFactories;
+
+    internal sealed class SurgicalDurationExport : ISurgicalDurationExport
+    {
+        public SurgicalDurationExport()
+        {
+        }
+
+        public ISurgicalDurationOutputContext GetSurgicalDuration(
+            IAbstractFactory abstractFactory,
+            ISurgicalDurationInputContext surgicalDurationInputContext)
+        {
+            return abstractFactory.CreateContextsAbstractFactory().CreateSurgicalDurationOutputContextFactory().Create(
+                abstractFactory.CreateSurgicalDurationsAbstractFactory()
+                .CreateSurgicalDurationFactory(
+                    surgicalDurationInputContext.Specialty)
+                .Create(
+                    abstractFactory.CreateDependenciesAbstractFactory().CreateDurationFactory())
+                .GetValue(
+                    surgicalDurationInputContext.Statistic));
+        }
+    }
+}
---
+++ VanOostrum2011/Interfaces/Exports/SurgicalDurations/ISurgicalDurationExport.cs
@@ -0,0 +1,13 @@
+﻿namespace VanOostrum2011.Interfaces.Exports.SurgicalDurations
+{
+    using VanOostrum2011.Interfaces.Contexts.SurgicalDurations;
+    using VanOostrum2011.Interfaces.Exports.SurgicalDurations;
+    using VanOostrum2011.InterfacesAbstractFactories;
+
+    public interface ISurgicalDurationExport
+    {
+        ISurgicalDurationOutputContext GetSurgicalDuration(
+            IAbstractFactory abstractFactory,
+            ISurgicalDurationInputContext surgicalDurationInputContext);
+    }
+}
