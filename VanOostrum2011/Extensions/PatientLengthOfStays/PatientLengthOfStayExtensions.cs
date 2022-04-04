f043b6efdd0e69b589542ac74f70f7ad93ca7cb2
Justin Britt <britt@uwindsor.ca>
2022-04-04 17:40:00 -0400

    Added class

---
+++ VanOostrum2011/Extensions/PatientLengthOfStays/PatientLengthOfStayExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace VanOostrum2011.Extensions.PatientLengthOfStays
+{
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.Interfaces.PatientLengthOfStays;
+
+    public static class PatientLengthOfStayExtensions
+    {
+        private const string average = "average";
+        private const string stddev = "std-dev";
+
+        public static Duration GetValue(
+            this IPatientLengthOfStay patientLengthOfStay,
+            IValue<string> statistic)
+        {
+            return statistic.Value switch
+            {
+                average => patientLengthOfStay.Mean,
+
+                stddev => patientLengthOfStay.StandardDeviation,
+
+                _ => null
+            };
+        }
+    }
+}
