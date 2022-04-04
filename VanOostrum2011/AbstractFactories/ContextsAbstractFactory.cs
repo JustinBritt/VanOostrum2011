c93ae1553af651f49a985f4081648cc4e20c503c
Justin Britt <britt@uwindsor.ca>
2022-04-04 16:15:26 -0400

    Added abstract factory

---
+++ VanOostrum2011/AbstractFactories/ContextsAbstractFactory.cs
@@ -0,0 +1,75 @@
+﻿namespace VanOostrum2011.AbstractFactories
+{
+    using VanOostrum2011.Factories.Contexts.PatientLengthOfStays;
+    using VanOostrum2011.Factories.Contexts.SurgicalDurations;
+    using VanOostrum2011.InterfacesAbstractFactories;
+    using VanOostrum2011.InterfacesFactories.Contexts.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesFactories.Contexts.SurgicalDurations;
+
+    internal sealed class ContextsAbstractFactory : IContextsAbstractFactory
+    {
+        public ContextsAbstractFactory()
+        {
+        }
+
+        public IPatientLengthOfStayInputContextFactory CreatePatientLengthOfStayInputContextFactory()
+        {
+            IPatientLengthOfStayInputContextFactory factory = null;
+
+            try
+            {
+                factory = new PatientLengthOfStayInputContextFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public IPatientLengthOfStayOutputContextFactory CreatePatientLengthOfStayOutputContextFactory()
+        {
+            IPatientLengthOfStayOutputContextFactory factory = null;
+
+            try
+            {
+                factory = new PatientLengthOfStayOutputContextFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public ISurgicalDurationInputContextFactory CreateSurgicalDurationInputContextFactory()
+        {
+            ISurgicalDurationInputContextFactory factory = null;
+
+            try
+            {
+                factory = new SurgicalDurationInputContextFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public ISurgicalDurationOutputContextFactory CreateSurgicalDurationOutputContextFactory()
+        {
+            ISurgicalDurationOutputContextFactory factory = null;
+
+            try
+            {
+                factory = new SurgicalDurationOutputContextFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+    }
+}
---
+++ VanOostrum2011/InterfacesAbstractFactories/IContextsAbstractFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace VanOostrum2011.InterfacesAbstractFactories
+{
+    using VanOostrum2011.InterfacesFactories.Contexts.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesFactories.Contexts.SurgicalDurations;
+
+    public interface IContextsAbstractFactory
+    {
+        IPatientLengthOfStayInputContextFactory CreatePatientLengthOfStayInputContextFactory();
+
+        IPatientLengthOfStayOutputContextFactory CreatePatientLengthOfStayOutputContextFactory();
+
+        ISurgicalDurationInputContextFactory CreateSurgicalDurationInputContextFactory();
+
+        ISurgicalDurationOutputContextFactory CreateSurgicalDurationOutputContextFactory();
+    }
+}
