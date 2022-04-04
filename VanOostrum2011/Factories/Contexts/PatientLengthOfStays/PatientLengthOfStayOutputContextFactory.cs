0e4a75f729d1dcc5996f098fa77f4cda6afe7b02
Justin Britt <britt@uwindsor.ca>
2022-04-03 21:16:34 -0400

    Added factory

---
+++ VanOostrum2011/Factories/Contexts/PatientLengthOfStays/PatientLengthOfStayOutputContextFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace VanOostrum2011.Factories.Contexts.PatientLengthOfStays
+{
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.Classes.Contexts.PatientLengthOfStays;
+    using VanOostrum2011.Interfaces.Contexts.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesFactories.Contexts.PatientLengthOfStays;
+
+    internal sealed class PatientLengthOfStayOutputContextFactory : IPatientLengthOfStayOutputContextFactory
+    {
+        public PatientLengthOfStayOutputContextFactory()
+        {
+        }
+
+        public IPatientLengthOfStayOutputContext Create(
+            Duration duration)
+        {
+            IPatientLengthOfStayOutputContext context = null;
+
+            try
+            {
+                context = new PatientLengthOfStayOutputContext(
+                    duration);
+            }
+            finally
+            {
+            }
+
+            return context;
+        }
+    }
+}
---
+++ VanOostrum2011/InterfacesFactories/Contexts/PatientLengthOfStays/IPatientLengthOfStayOutputContextFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace VanOostrum2011.InterfacesFactories.Contexts.PatientLengthOfStays
+{
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.Interfaces.Contexts.PatientLengthOfStays;
+
+    public interface IPatientLengthOfStayOutputContextFactory
+    {
+        IPatientLengthOfStayOutputContext Create(
+            Duration duration);
+    }
+}
