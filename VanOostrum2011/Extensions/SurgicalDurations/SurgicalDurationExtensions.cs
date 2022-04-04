6297f24bd8cbb185be9bf75868be559f611eeeed
Justin Britt <britt@uwindsor.ca>
2022-04-04 17:37:21 -0400

    Added class

---
+++ VanOostrum2011/Extensions/SurgicalDurations/SurgicalDurationExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace VanOostrum2011.Extensions.SurgicalDurations
+{
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.Interfaces.SurgicalDurations;
+
+    public static class SurgicalDurationExtensions
+    {
+        private const string average = "average";
+        private const string stddev = "std-dev";
+
+        public static Duration GetValue(
+            this ISurgicalDuration surgicalDuration,
+            IValue<string> statistic)
+        {
+            return statistic.Value switch
+            {
+                average => surgicalDuration.Mean,
+
+                stddev => surgicalDuration.StandardDeviation,
+
+                _ => null
+            };
+        }
+    }
+}
