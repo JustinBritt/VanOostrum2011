3f51a72414d01b95fff5dff469065622b05e3e99
Justin Britt <britt@uwindsor.ca>
2022-04-04 17:33:08 -0400

    Added methods

--- VanOostrum2011/InterfacesAbstractFactories/ISurgicalDurationsAbstractFactory.cs
+++ VanOostrum2011/InterfacesAbstractFactories/ISurgicalDurationsAbstractFactory.cs
@@ -1,10 +1,28 @@
 ﻿namespace VanOostrum2011.InterfacesAbstractFactories
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using Hl7.Fhir.Model;
+
+    using VanOostrum2011.InterfacesFactories.SurgicalDurations;
 
     public interface ISurgicalDurationsAbstractFactory
     {
+        IEarNoseThroatSurgerySurgicalDurationFactory CreateEarNoseThroatSurgerySurgicalDurationFactory();
+
+        IEyeSurgerySurgicalDurationFactory CreateEyeSurgerySurgicalDurationFactory();
+
+        IGeneralSurgerySurgicalDurationFactory CreateGeneralSurgerySurgicalDurationFactory();
+
+        IGynecologySurgicalDurationFactory CreateGynecologySurgicalDurationFactory();
+
+        IOrthopedicSurgerySurgicalDurationFactory CreateOrthopedicSurgerySurgicalDurationFactory();
+
+        IOverallSurgicalDurationFactory CreateOverallSurgicalDurationFactory();
+
+        IPlasticSurgerySurgicalDurationFactory CreatePlasticSurgerySurgicalDurationFactory();
+
+        ISurgicalDurationFactory CreateSurgicalDurationFactory(
+            CodeableConcept specialty);
+
+        IUrologySurgicalDurationFactory CreateUrologySurgicalDurationFactory();
     }
 }
