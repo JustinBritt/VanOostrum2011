67f726207dd0c9405dbd00081a05d471d813aa93
Justin Britt <britt@uwindsor.ca>
2022-04-05 16:13:44 -0400

    Added test

--- VanOostrum2011.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
+++ VanOostrum2011.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -36,5 +36,30 @@ namespace VanOostrum2011.Tests.Classes.Exports.SurgicalDurations
                 expected: 27m,
                 actual: surgicalDurationOutputContext.Duration.Value.Value);
         }
+
+        [TestMethod]
+        public void EarNoseThroatSurgeryStdDev()
+        {
+            // Arrange
+            IAbstractFactory abstractFactory = AbstractFactory.Create();
+
+            IDependenciesAbstractFactory dependenciesAbstractFactory = abstractFactory.CreateDependenciesAbstractFactory();
+
+            ISurgicalDurationInputContext surgicalDurationInputContext = abstractFactory.CreateContextsAbstractFactory().CreateSurgicalDurationInputContextFactory().Create(
+                specialty: dependenciesAbstractFactory.CreateCodeableConceptFactory().CreateEarNoseThroatSurgery(),
+                statistic: dependenciesAbstractFactory.CreateValueFactory().CreateStdDev());
+
+            ISurgicalDurationExport surgicalDurationExport = abstractFactory.CreateExportsAbstractFactory().CreateSurgicalDurationExportFactory().Create();
+
+            // Act
+            ISurgicalDurationOutputContext surgicalDurationOutputContext = surgicalDurationExport.GetSurgicalDuration(
+                abstractFactory,
+                surgicalDurationInputContext);
+
+            // Assert
+            Assert.AreEqual(
+                expected: 29.8m,
+                actual: surgicalDurationOutputContext.Duration.Value.Value);
+        }
     }
 }
