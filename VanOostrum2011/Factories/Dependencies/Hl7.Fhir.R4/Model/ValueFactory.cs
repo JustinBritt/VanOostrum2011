eae61f9ef2b1fe451fd0d035a41b3d12330e3ef4
Justin Britt <britt@uwindsor.ca>
2022-04-05 13:50:52 -0400

    Added factory

---
+++ VanOostrum2011/Factories/Dependencies/Hl7.Fhir.R4/Model/ValueFactory.cs
@@ -0,0 +1,45 @@
+﻿namespace VanOostrum2011.Factories.Dependencies.Hl7.Fhir.R4.Model
+{
+    using global::Hl7.Fhir.Model;
+
+    using VanOostrum2011.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+
+    internal sealed class ValueFactory : IValueFactory
+    {
+        private const string average = "average";
+        private const string stddev = "std-dev";
+
+        public ValueFactory()
+        {
+        }
+
+        public IValue<string> Create(
+            string value)
+        {
+            IValue<string> instance = null;
+
+            try
+            {
+                instance = new FhirString(
+                    value);
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+
+        public IValue<string> CreateAverage()
+        {
+            return this.Create(
+                average);
+        }
+
+        public IValue<string> CreateStdDev()
+        {
+            return this.Create(
+                stddev);
+        }
+    }
+}
---
+++ VanOostrum2011/InterfacesFactories/Dependencies/Hl7.Fhir.R4/Model/IValueFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace VanOostrum2011.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model
+{
+    using global::Hl7.Fhir.Model;
+
+    public interface IValueFactory
+    {
+        IValue<string> Create(
+            string value);
+
+        IValue<string> CreateAverage();
+
+        IValue<string> CreateStdDev();
+    }
+}
