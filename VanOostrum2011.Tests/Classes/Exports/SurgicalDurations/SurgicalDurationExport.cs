ec4b939eeecc806a20df4f1ee3e5902b70631703
Justin Britt <britt@uwindsor.ca>
2022-04-05 18:56:36 -0400

    Added test

--- VanOostrum2011.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
+++ VanOostrum2011.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -190,6 +190,31 @@ namespace VanOostrum2011.Tests.Classes.Exports.SurgicalDurations
         }
 
         [TestMethod]
+        public void GynecologyStdDev()
+        {
+            // Arrange
+            IAbstractFactory abstractFactory = AbstractFactory.Create();
+
+            IDependenciesAbstractFactory dependenciesAbstractFactory = abstractFactory.CreateDependenciesAbstractFactory();
+
+            ISurgicalDurationInputContext surgicalDurationInputContext = abstractFactory.CreateContextsAbstractFactory().CreateSurgicalDurationInputContextFactory().Create(
+                specialty: dependenciesAbstractFactory.CreateCodeableConceptFactory().CreateGynecology(),
+                statistic: dependenciesAbstractFactory.CreateValueFactory().CreateStdDev());
+
+            ISurgicalDurationExport surgicalDurationExport = abstractFactory.CreateExportsAbstractFactory().CreateSurgicalDurationExportFactory().Create();
+
+            // Act
+            ISurgicalDurationOutputContext surgicalDurationOutputContext = surgicalDurationExport.GetSurgicalDuration(
+                abstractFactory,
+                surgicalDurationInputContext);
+
+            // Assert
+            Assert.AreEqual(
+                expected: 43.8m,
+                actual: surgicalDurationOutputContext.Duration.Value.Value);
+        }
+
+        [TestMethod]
         public void InvalidSurgicalSpecialtyAverage()
         {
             // Arrange
