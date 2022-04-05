77dd6f45b17f207400c81e6d5c15d0887adc584b
Justin Britt <britt@uwindsor.ca>
2022-04-04 22:53:45 -0400

    Added class

---
+++ VanOostrum2011/Classes/Exports/PatientLengthOfStays/PatientLengthOfStayExport.cs
@@ -0,0 +1,28 @@
+﻿namespace VanOostrum2011.Classes.Exports.PatientLengthOfStays
+{
+    using VanOostrum2011.Extensions.PatientLengthOfStays;
+    using VanOostrum2011.Interfaces.Contexts.PatientLengthOfStays;
+    using VanOostrum2011.Interfaces.Exports.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesAbstractFactories;
+
+    internal sealed class PatientLengthOfStayExport : IPatientLengthOfStayExport
+    {
+        public PatientLengthOfStayExport()
+        {
+        }
+
+        public IPatientLengthOfStayOutputContext GetPatientLengthOfStay(
+            IAbstractFactory abstractFactory,
+            IPatientLengthOfStayInputContext patientLengthOfStayInputContext)
+        {
+            return abstractFactory.CreateContextsAbstractFactory().CreatePatientLengthOfStayOutputContextFactory().Create(
+                abstractFactory.CreatePatientLengthOfStaysAbstractFactory()
+                .CreatePatientLengthOfStayFactory(
+                    patientLengthOfStayInputContext.Specialty)
+                .Create(
+                    abstractFactory.CreateDependenciesAbstractFactory().CreateDurationFactory())
+                .GetValue(
+                    patientLengthOfStayInputContext.Statistic));
+        }
+    }
+}
---
+++ VanOostrum2011/Interfaces/Exports/PatientLengthOfStays/IPatientLengthOfStayExport.cs
@@ -0,0 +1,12 @@
+﻿namespace VanOostrum2011.Interfaces.Exports.PatientLengthOfStays
+{
+    using VanOostrum2011.Interfaces.Contexts.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesAbstractFactories;
+
+    public interface IPatientLengthOfStayExport
+    {
+        IPatientLengthOfStayOutputContext GetPatientLengthOfStay(
+            IAbstractFactory abstractFactory,
+            IPatientLengthOfStayInputContext patientLengthOfStayInputContext);
+    }
+}
