edd8f7328a029642529e74d6e91fc4cb17aacd75
Justin Britt <britt@uwindsor.ca>
2022-04-05 12:01:54 -0400

    Added abstract factory

---
+++ VanOostrum2011/AbstractFactories/ExportsAbstractFactory.cs
@@ -0,0 +1,45 @@
+﻿namespace VanOostrum2011.AbstractFactories
+{
+    using VanOostrum2011.Factories.Exports.PatientLengthOfStays;
+    using VanOostrum2011.Factories.Exports.SurgicalDurations;
+    using VanOostrum2011.InterfacesAbstractFactories;
+    using VanOostrum2011.InterfacesFactories.Exports.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesFactories.Exports.SurgicalDurations;
+    
+    internal sealed class ExportsAbstractFactory : IExportsAbstractFactory
+    {
+        public ExportsAbstractFactory()
+        {
+        }
+
+        public IPatientLengthOfStayExportFactory CreatePatientLengthOfStayExportFactory()
+        {
+            IPatientLengthOfStayExportFactory factory = null;
+
+            try
+            {
+                factory = new PatientLengthOfStayExportFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+
+        public ISurgicalDurationExportFactory CreateSurgicalDurationExportFactory()
+        {
+            ISurgicalDurationExportFactory factory = null;
+
+            try
+            {
+                factory = new SurgicalDurationExportFactory();
+            }
+            finally
+            {
+            }
+
+            return factory;
+        }
+    }
+}
---
+++ VanOostrum2011/InterfacesAbstractFactories/IExportsAbstractFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace VanOostrum2011.InterfacesAbstractFactories
+{
+    using VanOostrum2011.InterfacesFactories.Exports.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesFactories.Exports.SurgicalDurations;
+
+    public interface IExportsAbstractFactory
+    {
+        IPatientLengthOfStayExportFactory CreatePatientLengthOfStayExportFactory();
+
+        ISurgicalDurationExportFactory CreateSurgicalDurationExportFactory();
+    }
+}
