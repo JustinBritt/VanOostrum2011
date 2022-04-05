57ad6bca4ffe7fe105b8c845c7cca10527efc306
Justin Britt <britt@uwindsor.ca>
2022-04-05 03:43:49 -0400

    Added factory

---
+++ VanOostrum2011/Factories/Exports/SurgicalDurations/SurgicalDurationExportFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanOostrum2011.Factories.Exports.SurgicalDurations
+{
+    using VanOostrum2011.Classes.Exports.SurgicalDurations;
+    using VanOostrum2011.Interfaces.Exports.SurgicalDurations;
+    using VanOostrum2011.InterfacesFactories.Exports.SurgicalDurations;
+
+    internal sealed class SurgicalDurationExportFactory : ISurgicalDurationExportFactory
+    {
+        public SurgicalDurationExportFactory()
+        {
+        }
+
+        public ISurgicalDurationExport Create()
+        {
+            ISurgicalDurationExport export = null;
+
+            try
+            {
+                export = new SurgicalDurationExport();
+            }
+            finally
+            {
+            }
+
+            return export;
+        }
+    }
+}
---
+++ VanOostrum2011/InterfacesFactories/Exports/SurgicalDurations/ISurgicalDurationExportFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace VanOostrum2011.InterfacesFactories.Exports.SurgicalDurations
+{
+    using VanOostrum2011.Interfaces.Exports.SurgicalDurations;
+
+    public interface ISurgicalDurationExportFactory
+    {
+        ISurgicalDurationExport Create();
+    }
+}
