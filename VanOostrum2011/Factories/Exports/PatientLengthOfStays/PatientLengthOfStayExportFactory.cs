41277bc96b40b908a8ce0a90613812ccd2aa6d3e
Justin Britt <britt@uwindsor.ca>
2022-04-05 03:39:23 -0400

    Added factory

---
+++ VanOostrum2011/Factories/Exports/PatientLengthOfStays/PatientLengthOfStayExportFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace VanOostrum2011.Factories.Exports.PatientLengthOfStays
+{
+    using VanOostrum2011.Classes.Exports.PatientLengthOfStays;
+    using VanOostrum2011.Interfaces.Exports.PatientLengthOfStays;
+    using VanOostrum2011.InterfacesFactories.Exports.PatientLengthOfStays;
+
+    internal sealed class PatientLengthOfStayExportFactory : IPatientLengthOfStayExportFactory
+    {
+        public PatientLengthOfStayExportFactory()
+        {
+        }
+
+        public IPatientLengthOfStayExport Create()
+        {
+            IPatientLengthOfStayExport export = null;
+
+            try
+            {
+                export = new PatientLengthOfStayExport();
+            }
+            finally
+            {
+            }
+
+            return export;
+        }
+    }
+}
---
+++ VanOostrum2011/InterfacesFactories/Exports/PatientLengthOfStays/IPatientLengthOfStayExportFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace VanOostrum2011.InterfacesFactories.Exports.PatientLengthOfStays
+{
+    using VanOostrum2011.Interfaces.Exports.PatientLengthOfStays;
+
+    public interface IPatientLengthOfStayExportFactory
+    {
+        IPatientLengthOfStayExport Create();
+    }
+}
