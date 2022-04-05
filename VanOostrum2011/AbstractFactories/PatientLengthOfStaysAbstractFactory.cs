c7d819713faedb82a022dcf7d930731920e12025
Justin Britt <britt@uwindsor.ca>
2022-04-04 21:35:02 -0400

    Added consts

--- VanOostrum2011/AbstractFactories/PatientLengthOfStaysAbstractFactory.cs
+++ VanOostrum2011/AbstractFactories/PatientLengthOfStaysAbstractFactory.cs
@@ -6,6 +6,17 @@
 
     internal sealed class PatientLengthOfStaysAbstractFactory : IPatientLengthOfStaysAbstractFactory
     {
+        // Codes
+        private const string EarNoseThroatSurgery = "394604002";
+        private const string GeneralSurgery = "394609007";
+        private const string GynecologicalSurgery = "394586005";
+        private const string OphthalmicSurgery = "422191005";
+        private const string Ophthalmology = "394594003";
+        private const string OrthopedicSurgery = "24241000087106";
+        private const string Overall = "394732004";
+        private const string PlasticSurgery = "394611003";
+        private const string Urology = "394612005";
+
         public PatientLengthOfStaysAbstractFactory()
         {
         }
