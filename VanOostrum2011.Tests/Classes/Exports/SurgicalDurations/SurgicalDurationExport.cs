afb383111638d71561430e96aea3146877a9084e
Justin Britt <britt@uwindsor.ca>
2022-04-05 18:13:58 -0400

    Added test

--- VanOostrum2011.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
+++ VanOostrum2011.Tests/Classes/Exports/SurgicalDurations/SurgicalDurationExport.cs
@@ -90,6 +90,31 @@ namespace VanOostrum2011.Tests.Classes.Exports.SurgicalDurations
         }
 
         [TestMethod]
+        public void EyeSurgeryStdDev()
+        {
+            // Arrange
+            IAbstractFactory abstractFactory = AbstractFactory.Create();
+
+            IDependenciesAbstractFactory dependenciesAbstractFactory = abstractFactory.CreateDependenciesAbstractFactory();
+
+            ISurgicalDurationInputContext surgicalDurationInputContext = abstractFactory.CreateContextsAbstractFactory().CreateSurgicalDurationInputContextFactory().Create(
+                specialty: dependenciesAbstractFactory.CreateCodeableConceptFactory().CreateEyeSurgery(),
+                statistic: dependenciesAbstractFactory.CreateValueFactory().CreateStdDev());
+
+            ISurgicalDurationExport surgicalDurationExport = abstractFactory.CreateExportsAbstractFactory().CreateSurgicalDurationExportFactory().Create();
+
+            // Act
+            ISurgicalDurationOutputContext surgicalDurationOutputContext = surgicalDurationExport.GetSurgicalDuration(
+                abstractFactory,
+                surgicalDurationInputContext);
+
+            // Assert
+            Assert.AreEqual(
+                expected: 10.3m,
+                actual: surgicalDurationOutputContext.Duration.Value.Value);
+        }
+
+        [TestMethod]
         public void InvalidSurgicalSpecialtyAverage()
         {
             // Arrange
