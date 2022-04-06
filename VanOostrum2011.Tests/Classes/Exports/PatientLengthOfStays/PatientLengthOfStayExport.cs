7f3caa9efc9d87686e02a4e645e470763897f66a
Justin Britt <britt@uwindsor.ca>
2022-04-06 02:57:08 -0400

    Added const

--- VanOostrum2011.Tests/Classes/Exports/PatientLengthOfStays/PatientLengthOfStayExport.cs
+++ VanOostrum2011.Tests/Classes/Exports/PatientLengthOfStays/PatientLengthOfStayExport.cs
@@ -16,6 +16,8 @@
     {
         private const string day = "d";
 
+        private const string skew = "skew";
+
         private const string SNOMEDCT = "http://snomed.info/sct";
 
         [TestMethod]
